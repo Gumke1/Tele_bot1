8bb62977bde6904dd2cf23961253d823554ed5a6
Dairmire <150476954+Dairmire@users.noreply.github.com>
2024-04-30 17:28:11 +0300

    Update main.py hhhhh

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram import Bot, Dispatcher, types
 
 async def main():
     db_session.global_init("db/blogs.db")
-    bot = Bot(token="6878332404:AAHJo2CtwBIg_IQAUG_Nwcn2XIBRXM1EN98")
+    bot = Bot(token="token")
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
